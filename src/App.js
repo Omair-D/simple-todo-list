3f22440691ef3ee9006f0d2d647e8daf1d7d192e
Omair-D <odurrani@kent.edu>
2024-03-28 03:54:55 -0400

    Added header and Input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      {/*1. Header */}
+      <h1>Simple Todo List</h1>
+
+      {/* 2. Input (input along with button) */}
+      <input
+      type ="text"
+      placeholder = 'Add an item'
+      />
+      {/* 3. Unordered list wiht list items */}
+
+
     </div>
   );
 }
